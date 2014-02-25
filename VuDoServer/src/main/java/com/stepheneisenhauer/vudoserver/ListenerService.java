f1291d459ea639ddbd60139785ffc5cca8127d19
Stephen Eisenhauer <bhs2007@gmail.com>
2014-02-25 04:07:03 -0600

    Improve HTTP Server and add semifunctional control panel page

--- VuDoServer/src/main/java/com/stepheneisenhauer/vudoserver/ListenerService.java
+++ VuDoServer/src/main/java/com/stepheneisenhauer/vudoserver/ListenerService.java
@@ -1,19 +1,17 @@
 package com.stepheneisenhauer.vudoserver;
 
-import android.annotation.TargetApi;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
 import android.net.Uri;
 import android.net.nsd.NsdManager;
 import android.net.nsd.NsdServiceInfo;
-import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.koushikdutta.async.http.body.AsyncHttpRequestBody;
 import com.koushikdutta.async.http.body.JSONObjectBody;
 import com.koushikdutta.async.http.server.AsyncHttpServer;
 import com.koushikdutta.async.http.server.AsyncHttpServerRequest;
@@ -23,11 +21,15 @@ import com.koushikdutta.async.http.server.HttpServerRequestCallback;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 
 /**
- * Created by stephen on 6/15/13.
+ * The ListenerService Service operates an HTTP server which listens for certain HTTP requests
+ * and takes appropriate action when they are received (e.g. opening a URL sent from a client).
  */
 public class ListenerService extends Service {
     static final String TAG = "VuDo/ListenerService";
@@ -38,6 +40,15 @@ public class ListenerService extends Service {
     NsdManager mNsdManager;
     NsdManager.RegistrationListener mRegistrationListener;
     Handler handler;
+
+    static final String TYPE_TEXT = "text/plain";
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // We want this service to continue running until it is explicitly
+        // stopped, so return sticky.
+        return START_STICKY;
+    }
 
     @Override
     public void onCreate() {
@@ -57,7 +68,21 @@ public class ListenerService extends Service {
         server.get("/", new HttpServerRequestCallback() {
             @Override
             public void onRequest(AsyncHttpServerRequest request, AsyncHttpServerResponse response) {
-                response.send("VuDo: Receive is active.");
+                try {
+                    // Serve the index.html page
+                    InputStream indexStream = getResources().openRawResource(R.raw.index);
+                    BufferedReader r = new BufferedReader(new InputStreamReader(indexStream));
+                    StringBuilder total = new StringBuilder();
+                    String line;
+                    while ((line = r.readLine()) != null) {
+                        total.append(line);
+                    }
+                    response.send(total.toString());
+                    return;
+                } catch (IOException e) {
+                    response.responseCode(500);
+                    response.send(TYPE_TEXT, "There was a problem serving this page :(");
+                }
             }
         });
         server.post("/view", new HttpServerRequestCallback() {
@@ -69,11 +94,14 @@ public class ListenerService extends Service {
                 try {
                     String type = json.getString("type");
                     if (type.equals("uri")) {
+                        // Open the URI using an ACTION_VIEW Intent
                         Uri uri = Uri.parse(json.getString("uri"));
                         Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                         intent.setComponent(null);
                         startActivity(intent);
+                        response.send(TYPE_TEXT, "The URI has been opened on the device.");
+                        return;
                     }
                     else if (type.equals("text")) {
                         // Display text as a Toast notification
@@ -84,20 +112,25 @@ public class ListenerService extends Service {
                                toast.show();
                            }
                         });
-                        response.send("");
+                        response.send(TYPE_TEXT, "The message has been displayed on the device.");
+                        return;
                     }
                     else if (type.equals("image")) {
-
+                        // TODO
+                        response.responseCode(400);
+                        response.send(TYPE_TEXT, "This content type isn't supported yet.");
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
+                // If we've reached this point, the user must have sent us something we don't know
+                // how to handle. Return a HTTP 400 response.
                 response.responseCode(400);
-                response.send("");
+                response.send(TYPE_TEXT, "Your request was bad, and you should _feel_ bad.");
             }
         });
 
-        // Start listening on port 5000
+        // Start listening on the selected port
         server.listen(mLocalPort);
         Log.d(TAG, "Listening on port " + mLocalPort);
 
@@ -127,11 +160,13 @@ public class ListenerService extends Service {
             public void onServiceUnregistered(NsdServiceInfo arg0) {
                 // Service has been unregistered.  This only happens when you call
                 // NsdManager.unregisterService() and pass in this listener.
+                Log.d(TAG, "Successfully unregistered network service.");
             }
 
             @Override
             public void onUnregistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
                 // Unregistration failed.  Put debugging code here to determine why.
+                Log.e(TAG, "Network service unregistration failed! Error code: " + errorCode);
             }
         };
     }
@@ -145,9 +180,16 @@ public class ListenerService extends Service {
         serviceInfo.setServiceType(mServiceType);
         serviceInfo.setPort(mLocalPort);
 
-        // Register the discovery service using the NSD Manager
+        // Grab the NSD Manager instance
         mNsdManager = (NsdManager) this.getSystemService(Context.NSD_SERVICE);
+
+        // Register the discovery service using the NSD Manager
         mNsdManager.registerService(serviceInfo, NsdManager.PROTOCOL_DNS_SD, mRegistrationListener);
+    }
+
+    private void unregisterNetworkService() {
+        mNsdManager.unregisterService(mRegistrationListener);
+        Toast.makeText(this, "VuDo: Receive is stopping...", Toast.LENGTH_SHORT).show();
     }
 
 
@@ -155,7 +197,11 @@ public class ListenerService extends Service {
         return null;
     }
 
+    @Override
     public void onDestroy() {
-        mNsdManager.unregisterService(mRegistrationListener);
+        super.onDestroy();
+        Log.d(TAG, "ListenerService is being destroyed.");
+        server.stop();
+        unregisterNetworkService();
     }
 }
